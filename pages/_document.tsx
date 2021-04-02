cf9f12ca4d82ec630f5134ae9fef65a206ff4a0b
Francesco <francescogmuller@hotmail.com>
2021-04-02 16:10:34 -0300

    init and config the projecty

---
+++ pages/_document.tsx
@@ -0,0 +1,22 @@
+import Document, { Html, NextScript, Main, Head } from 'next/document'
+
+export default class MyDocument extends Document {
+  render(): JSX.Element {
+    return (
+      <Html lang="pt-br">
+        <Head>
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap"
+            rel="stylesheet"
+          />
+          <link rel="shortcut icon" type="image/svg+xml" href="/favicon.svg" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
