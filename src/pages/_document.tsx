a503409dddce11849f9cefdca117465990158020
Francesco <francescogmuller@hotmail.com>
2021-05-08 00:50:46 -0300

    feature: add thumbnail to url links

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -15,6 +15,24 @@ export default class MyDocument extends Document {
             rel="stylesheet"
           />
           <link rel="shortcut icon" type="image/svg+xml" href="/favicon.svg" />
+
+          <meta name="description" content="Worldtrip" />
+
+          <meta property="og:site_name" content="Worldtrip" />
+
+          <meta property="og:title" content="Worldtrip" />
+          <meta property="og:description" content="Worldtrip" />
+
+          <meta property="og:image" content="/images/thumb.png" />
+          <meta property="og:image:type" content="image/png" />
+
+          <meta property="og:image:width" content="1200" />
+          <meta property="og:image:height" content="630" />
+
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:title" content="Worldtrip" />
+          <meta name="twitter:description" content="Worldtrip" />
+          <meta name="twitter:image" content="/images/thumb.png" />
         </Head>
         <body>
           <Main />
